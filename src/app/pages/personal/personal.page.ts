5eabfa8d464b435b82dd52ea9a0cc5b9fe4a1aaf
AnngieOspina <ancospinasu@unal.edu.co>
2020-07-16 19:51:08 -0500

    Act4

--- src/app/pages/personal/personal.page.ts
+++ src/app/pages/personal/personal.page.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal.page.scss'],
 })
 export class PersonalPage implements OnInit {
-
+  componentes: Componente[] = [
+    {
+      icon:'alarm-outline',
+      name:'Recordatorios',
+      redirectTo: '/recordatorio'
+   },
+   {
+      icon:'calendar-outline',
+      name:'Agenda',
+      redirectTo: '/agenda'
+    },
+    {
+      icon:'book-outline',
+      name:'Mi Diario',
+      redirectTo: '/diario'
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+interface Componente{
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
